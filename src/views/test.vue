80888700a43c7e7a7e73463bd1dcb036e4f68cd3
彭涛 <pengtaonumber@gmail.com>
2021-01-06 14:39:46 +0800

    新增dev测试模板

---
+++ src/views/test.vue
@@ -0,0 +1,21 @@
+<template>
+<div>我是新增测试模板</div>
+</template>
+
+<script>
+export default {
+    name: '',
+    components: {},
+    props: {},
+    data () {
+        return {}
+    },
+    computed: {},
+    watch: {
+    },
+    created () {},
+    methods: {}
+}
+</script>
+
+<style scoped></style>
